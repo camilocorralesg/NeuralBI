5f99e7a0f74a52c7bb4a655f84d8719c1cfcdeba
Camilo Corrales <camilocorralesg@users.noreply.github.com>
2026-07-26 16:13:16 -0500

    fix: complete component modularization, resolve missing variables, update industries copy

--- components/sections/Footer.jsx
+++ components/sections/Footer.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef, useCallback, useMemo, memo } from 'react';
 
-const logoUrl = 'https://cdn.dribbble.com/userupload/45579979/file/original-e39fe7bbbf27a199d799042b31174ef8.png?resize=1504x1128&vertical=center';
+const logoUrl = '/assets/Neuralbi logo.svg';
 
 
 
